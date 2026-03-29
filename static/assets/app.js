86ae477b028977895f212e54b5fe7db20f130c89
Nour <1746181+n0ur@users.noreply.github.com>
2026-03-29 21:08:43 +0200

    draw a polygon and circle from addresses

--- static/assets/app.js
+++ static/assets/app.js
@@ -5,10 +5,11 @@ async function initMap() {
   }
 
   // load libraries
-  const [, geocoding, { AdvancedMarkerElement }] = await Promise.all([
+  const [, geocoding, { AdvancedMarkerElement }, geometry] = await Promise.all([
     google.maps.importLibrary("maps"),
     google.maps.importLibrary("geocoding"),
     google.maps.importLibrary("marker"),
+    google.maps.importLibrary("geometry"),
   ]);
 
   // html elements
@@ -18,9 +19,12 @@ async function initMap() {
   // arguments used in functions below
   const args = {
     addresses: new Map(),
+    polygon: null,
+    circle: null,
     response: document.getElementById("response"),
     mapElement,
     AdvancedMarkerElement,
+    geometry,
   };
 
   const geocoder = new geocoding.Geocoder();
@@ -30,6 +34,9 @@ async function initMap() {
       .then((response) => {
         const { results } = response;
         const first = results[0];
+        if (args.addresses.get(first.formatted_address)) {
+          return;
+        }
         addMarker(first.formatted_address, first.geometry.location, args);
       })
       .catch((e) => {
@@ -42,6 +49,8 @@ async function initMap() {
   document.getElementById("clear").addEventListener("click", () => {
     inputText.value = "";
   });
+
+  document.getElementById("find").addEventListener("click", () => {});
 
   navigator.geolocation.getCurrentPosition(
     (position) => {
@@ -74,25 +83,29 @@ function addMarker(label, pos, args) {
   addresses.set(label, marker);
   updateBounds(args);
   renderAddresses(args);
+  drawPolygon(args);
+}
+
+function removeMarker(item, args) {
+  const { addresses } = args;
+  if (addresses.size <= 1) {
+    console.error("Cannot delete last address, add a new one first");
+    return;
+  }
+  const marker = addresses.get(item);
+  marker.map = null;
+  addresses.delete(item);
+  updateBounds(args);
+  renderAddresses(args);
+  drawPolygon(args);
 }
 
 function renderAddresses(args) {
   const { response, addresses } = args;
   response.innerHTML = "";
 
-  const list = createList(
-    [...addresses].map((x) => x[0]),
-    (item) => {
-      if (addresses.size <= 1) {
-        console.error("Cannot delete last address, add a new one first");
-        return;
-      }
-      const marker = addresses.get(item);
-      marker.map = null;
-      addresses.delete(item);
-      updateBounds(args);
-      renderAddresses(args);
-    },
+  const list = createList([...addresses.keys()], (item) =>
+    removeMarker(item, args),
   );
   response.append(list);
 }
@@ -100,19 +113,23 @@ function renderAddresses(args) {
 function updateBounds({ mapElement, addresses }) {
   const bounds = new google.maps.LatLngBounds();
   for (const [, marker] of addresses) {
-    const pos = marker.position;
-    const lat = typeof pos.lat === "function" ? pos.lat() : pos.lat;
-    const lng = typeof pos.lng === "function" ? pos.lng() : pos.lng;
-    bounds.extend({ lat, lng });
+    bounds.extend(getPosition(marker));
   }
   // When there's only one marker, fitBounds() zooms to the maximum zoom level
   if (addresses.size === 1) {
     const pos = [...addresses.values()][0].position;
     mapElement.innerMap.setCenter(pos);
-    mapElement.innerMap.setZoom(15);
+    mapElement.innerMap.setZoom(13);
   } else {
     mapElement.innerMap.fitBounds(bounds);
   }
+}
+
+function getPosition(marker) {
+  const pos = marker.position;
+  const lat = typeof pos.lat === "function" ? pos.lat() : pos.lat;
+  const lng = typeof pos.lng === "function" ? pos.lng() : pos.lng;
+  return { lat, lng };
 }
 
 function createList(items, onClick) {
@@ -132,4 +149,58 @@ function handleLocationError(browserHasGeolocation) {
       ? "Geolocation permissions denied, using default location"
       : "Error: browser doesnt support geolocation",
   );
+}
+
+function drawPolygon(args) {
+  const { addresses, mapElement, geometry } = args;
+  if (addresses.size <= 1) {
+    return;
+  }
+
+  const positions = [...addresses.values()].map((v) => getPosition(v));
+  const last = positions[positions.length - 1];
+
+  if (args.polygon && geometry.poly.containsLocation(last, args.polygon)) {
+    console.log("new address is within polygon, not drawing");
+    return;
+  }
+  // TODO: try: find outer paths only, or sort points, or path union
+  const sorted = positions.sort((a, b) => {
+    return a.lat - b.lat;
+  });
+
+  if (args.polygon) {
+    args.polygon.setPaths(sorted);
+  } else {
+    args.polygon = new google.maps.Polygon({
+      paths: positions,
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map: mapElement.innerMap,
+    });
+  }
+
+  // compute area of polygon
+  const area = geometry.spherical.computeArea(sorted);
+  // convert polygon to circle
+  const radius = Math.sqrt(area / Math.PI);
+  const center = mapElement.innerMap.getBounds().getCenter();
+  if (args.circle) {
+    args.circle.setRadius(radius);
+    args.circle.setCenter(center);
+  } else {
+    args.circle = new google.maps.Circle({
+      center: center,
+      radius: radius,
+      strokeColor: "#00FF00",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#00FF00",
+      fillOpacity: 0.35,
+      map: mapElement.innerMap,
+    });
+  }
 }
