5386b3b275411d85d2794cff6db19c880afc312c
Nour <1746181+n0ur@users.noreply.github.com>
2026-04-02 15:11:17 +0200

    add a ws endpoint, better dev logs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
+import fastifyWs from "@fastify/websocket";
 import path from "node:path";
 import { client } from "./gmapsClient.js";
 import { readFileSync } from "node:fs";
 
+const isDev = process.env.NODE_ENV !== "production";
+
 const fastify = Fastify({
-  logger: {
-    level: "info",
-  },
+  logger: isDev
+    ? {
+        level: "warn",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+      }
+    : { level: "info" },
+});
+
+fastify.register(fastifyStatic, {
+  root: path.join(import.meta.dirname, "../static/assets"),
+});
+
+fastify.register(fastifyWs);
+
+fastify.register(async function (fastify) {
+  fastify.get("/addr", { websocket: true }, (socket, req) => {
+    socket.on("message", (message) => {
+      try {
+        const data = JSON.parse(message.toString());
+        console.log("message received", data);
+        //socket.send(JSON.stringify({ status: 'ok', data: 'hi from server' }))
+      } catch (e) {
+        console.error("Could not parse JSON:" + message.toString());
+      }
+    });
+  });
 });
 
 fastify.get("/", (req, res) => {
@@ -15,15 +46,12 @@ fastify.get("/", (req, res) => {
     path.join(import.meta.dirname, "../static/index.html"),
     "utf8",
   );
-  const injected = html.replace(
-    "{{GOOGLE_MAPS_API_KEY}}",
-    process.env.GOOGLE_MAPS_API_KEY || "",
-  );
-  res.type("text/html").send(injected);
-});
 
-fastify.register(fastifyStatic, {
-  root: path.join(import.meta.dirname, "../static/assets"),
+  const injected = html
+    .replace("{{GOOGLE_MAPS_API_KEY}}", process.env.GOOGLE_MAPS_API_KEY || "")
+    .replace("{{WS_ENDPOINT}}", process.env.WS_ENDPOINT || "");
+
+  res.type("text/html").send(injected);
 });
 
 fastify.get(
@@ -66,7 +94,8 @@ fastify.get(
 );
 
 try {
-  await fastify.listen({ port: 3000 });
+  const port = parseInt(process.env.SERVER_PORT, 10) || 3000;
+  await fastify.listen({ port });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
